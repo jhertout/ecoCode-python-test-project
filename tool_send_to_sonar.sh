3a97092b23eea6b5d23208c065ac0b1e1e53b4b1
David DE CARVALHO <ddecarvalho@solocal.com>
2023-03-29 23:23:22 +0200

    improve shell and doc

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 # "sonar.login" variable : private TOKEN generated in your local SonarQube during installation
-mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=sqa_ce65bbe4d4ab26a121a1f858af77641fde641971
+# (input paramater of this script)
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=$1
